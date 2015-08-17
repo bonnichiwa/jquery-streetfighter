f2ef758d29958af852530ce1fecc58a94c3a5188
bonnichiwa <bonnie.pham@ryerson.ca>
2015-08-17 19:52:08 -0400

    added hadouken sound and animation

--- js/app.js
+++ js/app.js
@@ -8,12 +8,25 @@ $(document).ready(function() {
     $('.ryu-still').show();
   })
   .mousedown(function() {
+    playHadouken();
     $('.ryu-ready').hide();
     $('.ryu-throwing').show();
-    $('.hadouken').show();
+    $('.hadouken').finish().show().animate(
+      {'left':'1115px'},
+      500,
+      function() {
+        $(this).hide();
+        $(this).css('left', '615px');
+      }
+    );
   })
   .mouseup(function() {
     $('.ryu-throwing').hide();
     $('.ryu-ready').show();
-  });
+  })
+  function playHadouken () {
+    $('#hadouken-sound')[0].volume = 0.5;
+    $('#hadouken-sound')[0].load();
+    $('#hadouken-sound')[0].play();
+  }
 });
